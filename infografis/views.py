138b1cb8c3f764ebdb4e151864d313ef66e18e78
Farras Hafizhudin <103520002+farrasjay@users.noreply.github.com>
2022-10-31 19:19:47 +0700

    merged & fixed login, user profile, infografis

--- infografis/views.py
+++ infografis/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from uhealths.models import *
+from infografis.models import *
 from django.views.decorators.csrf import csrf_exempt
 
 def infografis(request):

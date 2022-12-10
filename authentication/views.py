e56e45663eddd63f771f6cc6d881f3c06d8a3f04
Farras Hafizhudin <103520002+farrasjay@users.noreply.github.com>
2022-12-10 12:38:31 +0700

    init flutter's auth

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login as auth_login
-from django.http import JsonResponse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, logout, login as auth_login
+from django.contrib import messages
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import datetime
+import json
+
+def index(request):
+    return render(request, 'index.html')
 
 @csrf_exempt
-def login(request):
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('authentication:index')
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+@csrf_exempt
+def login_flutter(request):
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
@@ -14,7 +38,8 @@ def login(request):
             # Redirect to a success page.
             return JsonResponse({
             "status": True,
-            "message": "Successfully Logged In!"
+            "message": "Successfully Logged In!",
+            # "user_id": user.id,
             # Insert any extra data if you want to pass data to Flutter
             }, status=200)
         else:
@@ -28,3 +53,21 @@ def login(request):
         "status": False,
         "message": "Failed to Login, check your email/password."
         }, status=401)
+
+def logout_user(request):
+    logout(request)
+    return redirect('authentication:login')
+
+@csrf_exempt
+def register_user(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('authentication:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)

fc5e8cf8ce7c38327525eba872650eb3f0b6de56
Jihan Syafa Kamila <88375711+jihansyafa@users.noreply.github.com>
2022-12-12 11:15:16 +0700

    Update urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path('api/comment/<int:id>/', get_comment_list, name="getCommentList"),
     path('api/addComment/<int:id>/', create_comment_ajax, name="addNewComment"),
     path('deleteForum/<int:id>/', delete_forum, name="deleteForum"),
-    path('deleteComment/<int:id>/', delete_comment, name="deleteComment")
+    path('deleteComment/<int:id>/', delete_comment, name="deleteComment"),
+    path('discussionFlutter/', get_forum_flutter, name="getForumFlutter"),
+    path('commentFlutter/<int:id>/', get_comment_flutter, name="getCommentFlutter"),
+    path('flutter/addDiscussion/',  create_forum_flutter, name="flutterAddForum"),
+    path('flutter/addComment/<int:id>/', create_comment_flutter, name="flutterAddComment"),
 ]
